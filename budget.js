cc66b4efca396be05b16993148d6ed2a105094b3
johnjc153 <153johnjc@gmail.com>
2021-07-26 04:18:17 -0700

    budget.js doc selectors

---
+++ budget.js
@@ -0,0 +1,25 @@
+const incomeTotalE1 = document.querySelector(".outcome-total");
+
+const expenseBtn = document.querySelector(".tab1");
+const incomeBtn = document.querySelector(".tab2");
+const allBtn = document.querySelector(".tab3");
+
+const incomeList = document.querySelector("#income .list");
+const expenseList = document.querySelector("#expense .list");
+const allList = document.querySelector("#all .list");
+
+const addExpense = document.querySelector(".add-expense");
+const expenseTitle = document.getElementById("expense-title-input");
+const expenseAmount = document.getElementById("expense-amount-input");
+
+const balanceE1 = document.querySelector(".balance .value");
+const outcomeTotalE1 = document.querySelector(".income-total");
+const chartE1 = document.querySelector(".chart");
+
+const expenseE1 = document.querySelector("#expense");
+const incomeE1 = document.querySelector("#income");
+const allE1 = document.querySelector("#all");
+
+const addIncome = document.querySelector(".add-income");
+const incomeTitle = document.getElementById("income-title-input");
+const incomeAmount = document.getElementById("income-amount-input");
